fb3d95217d7e96775cd01cea580a2e31fce7ad0e
Sean Belling <seanbelling@Sean-Bellings-MacBook-Pro.local>
2014-02-01 17:30:27 -0800

    this is getting annoying

--- transportation-day-of.php
+++ transportation-day-of.php
@@ -2,10 +2,10 @@
   require_once 'header.php';
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<! &ndash;[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif] &ndash;>
+<! &ndash;[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif] &ndash;>
+<! &ndash;[if IE 8]>         <html class="no-js lt-ie9"> <![endif] &ndash;>
+<! &ndash;[if gt IE 8]><! &ndash;> <html class="no-js"> <! &ndash;<![endif] &ndash;>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,7 +13,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <! &ndash; Place favicon.ico and apple-touch-icon.png in the root directory  &ndash;>
 
         <link rel="stylesheet" href="/static/css/normalize.css">
         <link rel="stylesheet" href="/static/css/site.css">
@@ -43,23 +43,21 @@
 
 
                 <div class="clearfix"><div class="term"><h3>Taxi:</h3></div>
-                <div class="def"><p>Hires may be easy (call early!) for transport to the Swedenborgian, however taxi service from the Log Cabin is nearly impossible-- Taxis will not pick up passengers from the Presidio on Saturday nights. Ride-share services like <a href="https://www.uber.com/" target="_blank">Uber</a> and <a href="http://www.lyft.com/" target="_blank">Lyft</a> are highly recommended although their late-night availability from the Presidio is not guaranteed.</p></div></div>
+                <div class="def"><p>Hires may be easy (call early!) for transport to the Swedenborgian, however taxi service from the Log Cabin is nearly impossible &ndash; Taxis will not pick up passengers from the Presidio on Saturday nights. Ride-share services like <a href="https://www.uber.com/" target="_blank">Uber</a> and <a href="http://www.lyft.com/" target="_blank">Lyft</a> are highly recommended although their late-night availability from the Presidio is not guaranteed.</p></div></div>
 
                 <div class="clearfix"><div class="term"><h3>Public Transportation:</h3></div>
-                <div class="def"><p>The Swedenborgian lies one block from the MUNI <a href="https://www.sfmta.com/getting-around/transit/routes-stops/3-jackson" target="_blank">3</a> and <a href="https://www.sfmta.com/getting-around/transit/routes-stops/43-masonic" target="_blank">43</a> lines, and three blocks from the MUNI <a href="https://www.sfmta.com/getting-around/transit/routes-stops/1-california" target="_blank">1</a>, <a href="https://www.sfmta.com/getting-around/transit/routes-stops/2-clement" target="_blank">2</a>, and <a href="https://www.sfmta.com/getting-around/transit/routes-stops/24-divisadero" target="_blank">24</a> lines. Public transportation is not available from the Log Cabin. </p></div></div>
+                <div class="def"><p>The <a href="https://www.google.com/maps/preview?q=swedenborgian+church+san+francisco&oe=utf-8&client=firefox-a&fb=1&gl=us&hq=swedenborgian+church+san+francisco&cid=0,0,80599378669799465&t=m&z=16&iwloc=A" target="_blank">>Swedenborgian</a> lies one block from the MUNI <a href="https://www.sfmta.com/getting-around/transit/routes-stops/3-jackson" target="_blank">3</a> and <a href="https://www.sfmta.com/getting-around/transit/routes-stops/43-masonic" target="_blank">43</a> lines, and three blocks from the MUNI <a href="https://www.sfmta.com/getting-around/transit/routes-stops/1-california" target="_blank">1</a>, <a href="https://www.sfmta.com/getting-around/transit/routes-stops/2-clement" target="_blank">2</a>, and <a href="https://www.sfmta.com/getting-around/transit/routes-stops/24-divisadero" target="_blank">24</a> lines. Public transportation is not available from the Log Cabin. </p></div></div>
 
                 <div class="clearfix"><div class="term"><h3>Provided Transportation:</h3></div>
-                <div class="def"><p>We are providing a shuttle for out-of-town guests that will depart from the Hilton Union Square-- 333 O'Farrell Street-- at promptly 3pm to arrive at the Swedenborgian for a 4pm ceremony. After the ceremony, all guests will be transported to the Log Cabin for the reception. Shuttles will be available starting at 10pm to shuttle guests from the Log Cabin back to the Hilton Union Square, where taxis and public transportation are plentiful. </p></div></div>
+                <div class="def"><p>We are providing a shuttle for out-of-town guests that will depart from the Hilton Union Square &ndash; <a href="https://goo.gl/maps/TvMz8" target="_blank">333 O'Farrell Street</a> &ndash; at promptly 3pm to arrive at the Swedenborgian for a 4pm ceremony. After the ceremony, all guests will be transported to the <a href="http://goo.gl/maps/RLyKO" target="_blank">Log Cabin</a> for the reception. Shuttles will be available starting at 10pm to shuttle guests from the Log Cabin back to the Hilton Union Square, where taxis and public transportation are plentiful. </p></div></div>
 
                 <div class="clearfix"><div class="term"><h3>Our Suggestion:</h3></div>
-                <div class="def"><p><b>Out-of-Towners: Plan on being on the downtown shuttle at 3pm for a no-hassle transit to the ceremony. You will be transported from the ceremony to the reception. At the end of the reception (sad face) you will be transported back downtown where you can carry on wit' yo bad self.</b></p>
+                <div class="def"><p><b><i>Out-of-Towners</i>: Plan on being on the downtown shuttle at 3pm for no-hassle transit to the ceremony. You will be transported from the ceremony to the reception. At the end of the reception (sad face) you will be transported back downtown where you can carry on wit' yo bad self.</b></p>
 
-                <p><b>Locals: Allow enough time for walking, MUNI'ing or taxi/uber/lyft'ing to get your gorgeous self to the Swedenborgian for a 4pm ceremony. After the ceremony, ride our shuttle to the Log Cabin. After the reception, take our shuttle back downtown to carry on the debauchery or taxi/MUNI/BART back home :(.</b></p></div></div>
-
+                <p><b><i>Locals</i>: Allow enough time for walking, MUNI'ing or taxi/uber/lyft'ing to get your gorgeous self to the Swedenborgian for a 4pm ceremony. After the ceremony, ride our shuttle to the Log Cabin. After the reception, take our shuttle back downtown to carry on the debauchery or taxi/MUNI/BART back home (sad face).</b></p></div></div>
             </div>
           </section>
         </div>
-
 
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
