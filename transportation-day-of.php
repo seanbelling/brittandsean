b24e89d3e8ef52e1b50b606302240b88a3b1f0a7
Sean Belling <seanbelling@Sean-Bellings-MacBook-Pro.local>
2014-02-01 16:59:52 -0800

    ok

---
+++ transportation-day-of.php
@@ -0,0 +1,72 @@
+<?php
+  require_once 'header.php';
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>brittandsean.com</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="/static/css/normalize.css">
+        <link rel="stylesheet" href="/static/css/site.css">
+        <link href='http://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js"></script>
+
+
+        <script type="text/javascript">
+
+        </script>
+
+    </head>
+    <body>
+        <div id="content">
+          <header class="clearfix">
+            <h1>BRITT &amp; SEAN.</h1>
+            <?php echo printHeader(); ?>
+          </header>
+
+          <section id="main">
+            <div class="page-content">
+              <h2>Day of transportation</h2>
+              <h3>Driving:</h3>
+              <p>Street parking near the Swedenborgian is available but not easy. There are multiple small parking lots next to the Log Cabin. Driving will be your best bet if you do not plan on drinking and you need to make a quick exit after the reception ends at 11pm. </p>
+
+              <h3>Taxi:</h3>
+              <p>Hires may be easy (call early!) for transport to the Swedenborgian, however taxi service from the Log Cabin is nearly impossible-- Taxis will not pick up passengers from the Presidio on Saturday nights. Ride-share services like <a href="https://www.uber.com/" target="_blank">Uber</a> and <a href="http://www.lyft.com/" target="_blank">Lyft</a> are highly recommended although their late-night availability from the Presidio is not guaranteed.</p>
+
+              <h3>Public Transportation:</h3>
+              <p>The Swedenborgian lies one block from the MUNI <a href="https://www.sfmta.com/getting-around/transit/routes-stops/3-jackson" target="_blank">3</a> and <a href="https://www.sfmta.com/getting-around/transit/routes-stops/43-masonic" target="_blank">43</a> lines, and three blocks from the MUNI <a href="https://www.sfmta.com/getting-around/transit/routes-stops/1-california" target="_blank">1</a>, <a href="https://www.sfmta.com/getting-around/transit/routes-stops/2-clement" target="_blank">2</a>, and <a href="https://www.sfmta.com/getting-around/transit/routes-stops/24-divisadero" target="_blank">24</a> lines. Public transportation is not available from the Log Cabin. </p>
+
+              <h3>Provided Transportation:</h3>
+              <p>We are providing a shuttle for out-of-town guests that will depart from the Hilton Union Square-- 333 O'Farrell Street-- at promptly 3pm to arrive at the Swedenborgian for a 4pm ceremony. After the ceremony, all guests will be transported to the Log Cabin for the reception. Shuttles will be available starting at 10pm to shuttle guests from the Log Cabin back to the Hilton Union Square, where taxis and public transportation are plentiful. </p>
+
+              <h3>Our Suggestion:</h3>
+              <p><b>Out-of-Towners: Plan on being on the downtown shuttle at 3pm for a no-hassle transit to the ceremony. You will be transported from the ceremony to the reception. At the end of the reception (sad face) you will be transported back downtown where you can carry on wit' yo bad self.</b></p>
+
+              <p><b>Locals: Allow enough time for walking, MUNI'ing or taxi/uber/lyft'ing to get your gorgeous self to the Swedenborgian for a 4pm ceremony. After the ceremony, ride our shuttle to the Log Cabin. After the reception, take our shuttle back downtown to carry on the debauchery or taxi/MUNI/BART back home :(.</b></p>
+
+            </div>
+          </section>
+        </div>
+
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-44949795-1', 'brittandsean.com');
+          ga('send', 'pageview');
+        </script>
+    </body>
+</html>
---
+++ transportation-general.php
@@ -0,0 +1,57 @@
+<?php
+  require_once 'header.php';
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>brittandsean.com</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="/static/css/normalize.css">
+        <link rel="stylesheet" href="/static/css/site.css">
+        <link href='http://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js"></script>
+
+
+        <script type="text/javascript">
+
+        </script>
+
+    </head>
+    <body>
+        <div id="content">
+          <header class="clearfix">
+            <h1>BRITT &amp; SEAN.</h1>
+            <?php echo printHeader(); ?>
+          </header>
+
+          <section id="main">
+            <div class="page-content">
+              <h2>Things to do</h2>
+              <p>coming soon!!</p>
+
+            </div>
+          </section>
+        </div>
+
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-44949795-1', 'brittandsean.com');
+          ga('send', 'pageview');
+        </script>
+    </body>
+</html>
