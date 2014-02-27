7e7af33cc83e41aa1bba8d4d93a324e30306adda
Sean Belling <seanbelling@Sean-Bellings-MacBook-Pro.local>
2014-02-26 20:08:49 -0800

    ok cool

---
+++ tech.php
@@ -0,0 +1,76 @@
+<?php
+  require_once 'header.php';
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>brittandsean.com</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="/static/css/normalize.css">
+        <link rel="stylesheet" href="/static/css/site.css">
+        <link href='http://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js"></script>
+
+
+        <script type="text/javascript">
+
+        </script>
+
+    </head>
+    <body>
+        <div id="content">
+          <header class="clearfix">
+            <h1>BRITT &amp; SEAN.</h1>
+            <?php echo printHeader(); ?>
+          </header>
+
+          <section id="main">
+            <div class="page-content">
+              <h2>Tech</h2>
+              <h3>We want your help!</h3>
+              <p>There is so much to experience on our wedding weekend.
+                Although we may be in our own little bliss bubble, we want to see it all as you do.
+                Travel tribulations, bloody mary mornings, shuttle ride shenanigans,
+                first dance disasters, we want to see your pictures and video<sup>*</sup>.
+                At the end, we'll be able to create an awesome wedding film from the best source- you!
+              </p>
+              <h3>Here’s how to help:</h3>
+              <p>Download the free Storymix Media app from
+              <a href="http://itunes.apple.com/us/app/weddingmix/id457398126?mt=8" target="_blank">itunes</a>
+              and enter our password: Z5P5Q. You can shoot and upload pics and video instantaneously within
+              their app or at a later time when wi-fi connected. Thank you for helping us create some amazing memories!</p>
+
+              <h3><sup>*</sup>What? You didn’t think wedding sites would have fine print?</h3>
+              <p>Yes, now we are all super plugged-in and excited about collectively creating something awesome.
+              But during our ceremony, we want you to be truly present for this special time.
+              We will remind you to ignore everything you just read and actually turn off your cell
+              phones and put down your cameras. The photographers will capture how this moment looks —
+              we encourage you all to capture how it feels, without the distraction of technology.
+              After the ceremony of course, GAME ON.</p>
+
+            </div>
+          </section>
+        </div>
+
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-44949795-1', 'brittandsean.com');
+          ga('send', 'pageview');
+        </script>
+    </body>
+</html>
